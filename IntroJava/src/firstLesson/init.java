33828c78af1ea67a363c302c55c3c890e6977426
parod <parod@192.168.1.133>
2021-05-17 17:48:43 +0200

    HOME WORK 1

---
+++ IntroJava/src/firstLesson/init.java
@@ -0,0 +1,40 @@
+package firstLesson;
+
+public class init {
+
+	public static void main(String[] args) {
+		
+		int counter = 0;
+
+		
+		for (counter = 0; counter < 5; counter++) {
+			System.out.println(counter);
+		}
+
+		
+		for (counter = 1; counter <= 5; counter++) {
+			System.out.println(counter);
+		}
+
+		
+		for (counter = 1; counter <= 5; counter++) {
+			if (counter % 2 != 0) { 
+				System.out.println(counter);
+			}
+		}
+		
+
+		for (counter = 5; counter >= 1; counter--) {
+			System.out.println(counter);
+		}
+		
+		
+		for (counter = 0; counter <= 5; counter++) {
+			if (counter % 2 == 0) {
+				System.out.println(counter);
+			}
+		}
+
+	}
+
+}
